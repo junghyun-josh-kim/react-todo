0da713aea05469ec476fa49fae9064fd46c3e938
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-10-30 21:16:07 -0400

    JSX: Delete todo list

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,20 +1,29 @@
 import { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
 
 function TodoList() {
   const [todos, setTodos] = useState([
     { id: '0', text: 'TEXT', status: 'active' },
     { id: '1', text: 'TEXT TEXT', status: 'active' },
   ]);
-  const handleAdd = (todo) => {
-    setTodos([...todos, todo]);
-  };
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
+
   return (
     <section>
       <ul>
-        {todos.map((item) => {
-          return <li key={item.id}>{item.text}</li>;
-        })}
+        {todos.map((item) => (
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
       </ul>
       <AddTodo onAdd={handleAdd} />
     </section>
