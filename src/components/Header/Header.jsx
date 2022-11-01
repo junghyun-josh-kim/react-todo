32d2ecdce60d7d859fec8be136764cb8a17b013c
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-10-31 20:15:17 -0400

    TailywindCSS: Darkmode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
+import { useDarkMode } from '../../context/DarkModeContext';
 import styles from './Header.module.css';
+import { HiMoon, HiSun } from 'react-icons/hi';
 
 function Header({ filters, filter, onFilterChange }) {
+  const { darkMode, toggleDarkMode } = useDarkMode();
   return (
     <header className={styles.header}>
+      <button className={styles.toggle} onClick={toggleDarkMode}>
+        {!darkMode && <HiMoon />} {darkMode && <HiSun />}
+      </button>
+
       <ul className={styles.filters}>
         {filters.map((value, index) => {
           return (
